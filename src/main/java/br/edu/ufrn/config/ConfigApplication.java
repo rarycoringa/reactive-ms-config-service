7b3c02626a8040d80b45104f91e4a3348b9e235e
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-07-02 22:35:04 -0300

    create initial structure with spring boot and config server

---
+++ src/main/java/br/edu/ufrn/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package br.edu.ufrn.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
